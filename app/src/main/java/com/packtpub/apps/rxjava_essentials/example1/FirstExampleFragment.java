527f8119faa75b7e0f562e37f56be53c89b932b1
TechshinoEyeKey <zhi.wang@techshino.com>
2016-07-22 16:08:19 +0800

    update

--- app/src/main/java/com/packtpub/apps/rxjava_essentials/example1/FirstExampleFragment.java
+++ app/src/main/java/com/packtpub/apps/rxjava_essentials/example1/FirstExampleFragment.java
@@ -1,6 +1,5 @@
 package com.packtpub.apps.rxjava_essentials.example1;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,6 +24,8 @@ import com.packtpub.apps.rxjava_essentials.apps.AppInfo;
 import com.packtpub.apps.rxjava_essentials.apps.AppInfoRich;
 import com.packtpub.apps.rxjava_essentials.apps.ApplicationAdapter;
 import com.packtpub.apps.rxjava_essentials.apps.ApplicationsList;
+import com.trello.rxlifecycle.FragmentEvent;
+import com.trello.rxlifecycle.components.RxFragment;
 
 import java.io.File;
 import java.lang.reflect.Type;
@@ -38,125 +39,129 @@ import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class FirstExampleFragment extends Fragment {
+public class FirstExampleFragment extends RxFragment {
 
-    @Bind(R.id.fragment_first_example_list)
-    RecyclerView mRecyclerView;
-    @Bind(R.id.fragment_first_example_swipe_container)
-    SwipeRefreshLayout mSwipeRefreshLayout;
+  @Bind(R.id.fragment_first_example_list)
+  RecyclerView mRecyclerView;
+  @Bind(R.id.fragment_first_example_swipe_container)
+  SwipeRefreshLayout mSwipeRefreshLayout;
 
-    private ApplicationAdapter mAdapter;
-    private File mFilesDir;
+  private ApplicationAdapter mAdapter;
+  private File mFilesDir;
 
-    public FirstExampleFragment() {
-    }
+  public FirstExampleFragment() {
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
-        return inflater.inflate(R.layout.fragment_example, container, false);
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
+    return inflater.inflate(R.layout.fragment_example, container, false);
+  }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        ButterKnife.bind(this, view);
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ButterKnife.bind(this, view);
 
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
-        mAdapter = new ApplicationAdapter(new ArrayList<>(), R.layout.applications_list_item);
-        mRecyclerView.setAdapter(mAdapter);
+    mAdapter = new ApplicationAdapter(new ArrayList<>(), R.layout.applications_list_item);
+    mRecyclerView.setAdapter(mAdapter);
 
-        mSwipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.myPrimaryColor));
-        mSwipeRefreshLayout.setProgressViewOffset(false, 0,
-                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24,
-                        getResources().getDisplayMetrics()));
+    mSwipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.myPrimaryColor));
+    mSwipeRefreshLayout.setProgressViewOffset(false, 0,
+        (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24,
+            getResources().getDisplayMetrics()));
 
-        // Progress
-        mSwipeRefreshLayout.setEnabled(false);
-        mSwipeRefreshLayout.setRefreshing(true);
-        mRecyclerView.setVisibility(View.GONE);
+    // Progress
+    mSwipeRefreshLayout.setEnabled(false);
+    mSwipeRefreshLayout.setRefreshing(true);
+    mRecyclerView.setVisibility(View.GONE);
 
-        getFileDir().subscribeOn(Schedulers.io())
-                .subscribe(file -> {
-                    mFilesDir = file;
-                    refreshTheList();
-                });
-    }
-
-    private Observable<File> getFileDir() {
-        return Observable.create(subscriber -> {
-            subscriber.onNext(App.instance.getFilesDir());
-            subscriber.onCompleted();
+    getFileDir().subscribeOn(Schedulers.io())
+        .subscribe(file -> {
+          mFilesDir = file;
+          refreshTheList();
         });
-    }
+  }
 
-    private void refreshTheList() {
-        getApps().toSortedList().observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<List<AppInfo>>() {
-            @Override
-            public void onCompleted() {
-                Toast.makeText(getActivity(), "Here is the list!", Toast.LENGTH_LONG).show();
-            }
+  private Observable<File> getFileDir() {
+    return Observable.create(subscriber -> {
+      subscriber.onNext(App.instance.getFilesDir());
+      subscriber.onCompleted();
+    });
+  }
 
-            @Override
-            public void onError(Throwable e) {
-                Toast.makeText(getActivity(), "Something went wrong!", Toast.LENGTH_SHORT).show();
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
+  private void refreshTheList() {
+    getApps()
+        .toSortedList()
+        .observeOn(AndroidSchedulers.mainThread())
+        .compose(this.bindUntilEvent(FragmentEvent.DESTROY_VIEW))
+        .subscribe(new Observer<List<AppInfo>>() {
+          @Override
+          public void onCompleted() {
+            Toast.makeText(getActivity(), "Here is the list!", Toast.LENGTH_LONG).show();
+          }
 
-            @Override
-            public void onNext(List<AppInfo> appInfos) {
-                mRecyclerView.setVisibility(View.VISIBLE);
-                mAdapter.addApplications(appInfos);
-                mSwipeRefreshLayout.setRefreshing(false);
-                storeList(appInfos);
-            }
+          @Override
+          public void onError(Throwable e) {
+            Toast.makeText(getActivity(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+            mSwipeRefreshLayout.setRefreshing(false);
+          }
+
+          @Override
+          public void onNext(List<AppInfo> appInfos) {
+            mRecyclerView.setVisibility(View.VISIBLE);
+            mAdapter.addApplications(appInfos);
+            mSwipeRefreshLayout.setRefreshing(false);
+            storeList(appInfos);
+          }
         });
-    }
+  }
 
-    private void storeList(List<AppInfo> appInfos) {
-        ApplicationsList.getInstance().setList(appInfos);
+  private void storeList(List<AppInfo> appInfos) {
+    ApplicationsList.getInstance().setList(appInfos);
 
-        Schedulers.io().createWorker().schedule(() -> {
-            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-            Type appInfoType = new TypeToken<List<AppInfo>>() {
-            }.getType();
-            sharedPref.edit().putString("APPS", new Gson().toJson(appInfos, appInfoType)).apply();
-        });
-    }
+    Schedulers.io().createWorker().schedule(() -> {
+      SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+      Type appInfoType = new TypeToken<List<AppInfo>>() {
+      }.getType();
+      sharedPref.edit().putString("APPS", new Gson().toJson(appInfos, appInfoType)).apply();
+    });
+  }
 
-    private Observable<AppInfo> getApps() {
-        return Observable.create(subscriber -> {
-            List<AppInfoRich> apps = new ArrayList<>();
+  private Observable<AppInfo> getApps() {
+    return Observable.create(subscriber -> {
+      List<AppInfoRich> apps = new ArrayList<>();
 
-            final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+      final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+      mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-            List<ResolveInfo> infos =
-                    getActivity().getPackageManager().queryIntentActivities(mainIntent, 0);
-            for (ResolveInfo info : infos) {
-                apps.add(new AppInfoRich(getActivity(), info));
-            }
+      List<ResolveInfo> infos =
+          getActivity().getPackageManager().queryIntentActivities(mainIntent, 0);
+      for (ResolveInfo info : infos) {
+        apps.add(new AppInfoRich(getActivity(), info));
+      }
 
-            for (AppInfoRich appInfo : apps) {
-                Bitmap icon = Utils.drawableToBitmap(appInfo.getIcon());
-                String name = appInfo.getName();
-                String iconPath = mFilesDir + "/" + name;
-                Utils.storeBitmap(App.instance, icon, name);
+      for (AppInfoRich appInfo : apps) {
+        Bitmap icon = Utils.drawableToBitmap(appInfo.getIcon());
+        String name = appInfo.getName();
+        String iconPath = mFilesDir + "/" + name;
+        Utils.storeBitmap(App.instance, icon, name);
 
-                if (subscriber.isUnsubscribed()) {
-                    return;
-                }
-                subscriber.onNext(new AppInfo(name, iconPath, appInfo.getLastUpdateTime()));
-            }
-            if (!subscriber.isUnsubscribed()) {
-                subscriber.onCompleted();
-            }
-        });
-    }
+        if (subscriber.isUnsubscribed()) {
+          return;
+        }
+        subscriber.onNext(new AppInfo(name, iconPath, appInfo.getLastUpdateTime()));
+      }
+      if (!subscriber.isUnsubscribed()) {
+        subscriber.onCompleted();
+      }
+    });
+  }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ButterKnife.unbind(this);
-    }
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    ButterKnife.unbind(this);
+  }
 }
